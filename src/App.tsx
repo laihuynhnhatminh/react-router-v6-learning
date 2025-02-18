11c8f2b89cb9ca904923ff041dfb0e027be89f01
Minh Lai Huynh Nhat <nhatminhwork1996@gmail.com>
2025-02-18 22:42:59 +0700

    feat: create structured folder + css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Header from "./components/Header";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/about"} element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
